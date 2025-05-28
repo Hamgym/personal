94e5b7cd11b104aa015fc4258ce821857a20b2c6
Hamgym <pipiyakas@gmail.com>
2025-05-28 20:12:23 +0800

    update

--- models/rdb.py
+++ models/rdb.py
@@ -258,6 +258,8 @@ class CRUD:
   def read_brand(keyword, category, user_id):
     with cnxpool.get_connection() as cnx:
       cursor = cnx.cursor()
+      where = " WHERE TRUE"
+      value = []
       if user_id:
         select = """
           SELECT brand.name, COUNT(product.id) as count
@@ -266,31 +268,15 @@ class CRUD:
           JOIN brand ON product.brand=brand.id
           JOIN review ON product.id=review.product_id
         """
-        where = " WHERE TRUE"
-        value = []
         where += " AND review.user_id=%s"
         value.append(user_id)
-        if keyword!="":
-          where += " AND (product.name LIKE %s OR category.name LIKE %s OR brand.name LIKE %s)"
-          value.append(f"%{keyword}%")
-          value.append(f"%{keyword}%")
-          value.append(f"%{keyword}%")
-        if category!="" and category!="類別":
-          where += " AND category.name=%s"
-          value.append(category)
-        group = " GROUP BY brand.name"
-        order = " ORDER BY count DESC;"
-        cursor.execute(select+where+group+order, value)
-        rows = cursor.fetchall()
-        return rows
-      select = """
-        SELECT brand.name, COUNT(product.id) as count
-        FROM product
-        JOIN category ON product.category=category.id
-        JOIN brand ON product.brand=brand.id
-      """
-      where = " WHERE TRUE"
-      value = []
+      else:
+        select = """
+          SELECT brand.name, COUNT(product.id) as count
+          FROM product
+          JOIN category ON product.category=category.id
+          JOIN brand ON product.brand=brand.id
+        """
       if keyword!="":
         where += " AND (product.name LIKE %s OR category.name LIKE %s OR brand.name LIKE %s)"
         value.append(f"%{keyword}%")
