ecb2b19b3cf3eaa00fa65beb29913bde0f2c7f19
Hamgym <pipiyakas@gmail.com>
2025-05-31 22:49:03 +0800

    update

--- static/scripts/product.js
+++ static/scripts/product.js
@@ -294,12 +294,15 @@ async function init() {
     checkBox.addEventListener("change", function () {
       let icon = document.querySelector("img.personal");
       let searchBtn = document.querySelector(".header button");
+      let keyword = document.querySelector("#keyword");
       if (this.checked) {
         icon.src = "/static/images/user-checked.png";
+        keyword.value = "";
         searchBtn.click();
         showAlertBox("個人模式");
       } else {
         icon.src = "/static/images/user-unchecked.png";
+        keyword.value = "";
         searchBtn.click();
         showAlertBox("一般模式");
       }
