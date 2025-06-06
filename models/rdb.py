2194c3c334a33220e36cda8d89b0b774d5c199e1
Hamgym <pipiyakas@gmail.com>
2025-06-06 15:41:53 +0800

    update

--- models/rdb.py
+++ models/rdb.py
@@ -182,7 +182,7 @@ class CRUD:
     with cnxpool.get_connection() as cnx:
       cursor = cnx.cursor()
       select = """
-        SELECT DISTINCT product.id, category.name, brand.name, product.name, product.image, product.percent, product.review, lists.product_id
+        SELECT product.id, category.name, brand.name, product.name, product.image, product.percent, product.review, lists.product_id
         FROM product
         JOIN category ON product.category=category.id
         JOIN brand ON product.brand=brand.id
@@ -207,8 +207,9 @@ class CRUD:
         value.append(user_id)
       if sort!="percent" and sort!="review":
         sort = "id"
+      group = f" GROUP BY product.id"
       order = f" ORDER BY product.{sort} DESC"
-      cursor.execute(select+where+order, value)
+      cursor.execute(select+where+group+order, value)
       rows = cursor.fetchall()
       return rows
   def read_category(keyword, user_id):
